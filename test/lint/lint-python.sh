53fd1b3cc23637c55141647312c203f84bdee9d5
powers308 <powers_308@outlook.com>
2021-10-07 14:59:52 +1100

    Fixed spelling

--- test/lint/lint-python.sh
+++ test/lint/lint-python.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # Copyright (c) 2009-2019 The Bitcoin Core developers 
-# Copyright (c) 2019-2021 The Cerebral Core developers
+# Copyright (c) 2019-2021 The Cerebralcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #
